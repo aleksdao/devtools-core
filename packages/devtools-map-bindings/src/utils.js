7c1e2f883b7bd5e55b73c6a21344c981578b7cc1
Alexander Vu Dao <Alexander.V.Dao@aexp.com>
2017-11-03 00:24:58 -0400

    refactor map identifiers to names. add assertions

--- packages/devtools-map-bindings/src/utils.js
+++ packages/devtools-map-bindings/src/utils.js
@@ -2,9 +2,9 @@
 
 import type { MappedScopeBindings } from "debugger-html";
 const { default: generate } = require("babel-generator");
-
 const babylon = require("babylon");
 const { default: traverse } = require("babel-traverse");
+const t = require("babel-types");
 
 function replaceOriginalVariableName(
   expression: string,
@@ -12,19 +12,36 @@ function replaceOriginalVariableName(
 ): string {
   const ast = babylon.parse(expression, {
     sourceType: "module",
-    plugins: ["jsx", "flow", "objectRestSpread"]
+    plugins: ["jsx", "flow", "objectRestSpread"],
   });
 
   traverse(ast, {
-    Identifier(path) {
-      const { node: { name } } = path;
-      const foundScope = generatedScopes.find(
-        ({ bindings }) => name in bindings
-      );
-      if (foundScope) {
-        path.node.name = foundScope.bindings[name];
+    enter(path) {
+      const { parent } = path;
+      if (t.isMemberExpression(parent)) {
+        if (parent.computed || parent.object === path.node) {
+          const { node: { name } } = path;
+          const foundScope = generatedScopes.find(({ bindings }) => name in bindings);
+          if (foundScope) {
+            path.node.name = foundScope.bindings[name];
+          }
+        }
+      } else if (t.isExpressionStatement(parent)) {
+        const valuePath = path;
+
+        const { node: { name } } = path;
+        const foundScope = generatedScopes.find(({ bindings }) => name in bindings);
+        if (foundScope) {
+          valuePath.node.name = foundScope.bindings[name];
+        }
+      } else if (!t.isLabeledStatement(parent)) {
+        const { node: { name } } = path;
+        const foundScope = generatedScopes.find(({ bindings }) => name in bindings);
+        if (foundScope) {
+          path.node.name = foundScope.bindings[name];
+        }
       }
-    }
+    },
   });
 
   return generate(ast, { concise: true, compact: true }).code.replace(/;$/, "");
